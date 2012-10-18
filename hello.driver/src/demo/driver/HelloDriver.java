23c10ad75557660bbd05e621b439e8cb089a7a8e
Glyn Normington <gnormington@vmware.com>
2012-10-18 10:29:02 +0100

    start installed subsystem

--- hello.driver/src/demo/driver/HelloDriver.java
+++ hello.driver/src/demo/driver/HelloDriver.java
@@ -1,3 +1,4 @@
+
 package demo.driver;
 
 import java.io.File;
@@ -9,22 +10,30 @@ import org.osgi.service.subsystem.Subsystem;
 
 public class HelloDriver implements BundleActivator {
 
-	private ServiceReference<Subsystem> sr;
-	private Subsystem rootSubsystem;
+    private ServiceReference<Subsystem> sr;
 
-	@Override
-	public void start(BundleContext bc) throws Exception {
-		this.sr = (ServiceReference<Subsystem>) bc.getServiceReference(Subsystem.class);
-		this.rootSubsystem = bc.getService(this.sr);
-		this.rootSubsystem.install(new File("hello.esa").toURI().toString());
-	}
+    private Subsystem rootSubsystem;
 
-	@Override
-	public void stop(BundleContext bc) throws Exception {
-		if (this.sr != null) {
-			bc.ungetService(this.sr);
-			this.sr = null;
-		}
-	}
+    private Subsystem helloSubsystem;
+
+    @Override
+    public void start(BundleContext bc) throws Exception {
+        this.sr = bc.getServiceReference(Subsystem.class);
+        this.rootSubsystem = bc.getService(this.sr);
+        this.helloSubsystem = this.rootSubsystem.install(new File("hello.esa").toURI().toString());
+        this.helloSubsystem.start();
+    }
+
+    @Override
+    public void stop(BundleContext bc) throws Exception {
+        if (this.helloSubsystem != null) {
+            this.helloSubsystem.stop();
+            this.helloSubsystem = null;
+        }
+        if (this.sr != null) {
+            bc.ungetService(this.sr);
+            this.sr = null;
+        }
+    }
 
 }
